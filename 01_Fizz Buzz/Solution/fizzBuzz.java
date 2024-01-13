225edd4b4c43dc3d7eb298ea8c57ccacf38b90ff
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-13 14:25:31 +0530

    Update fizzBuzz.java

--- 01_Fizz Buzz/Solution/fizzBuzz.java
+++ 01_Fizz Buzz/Solution/fizzBuzz.java
@@ -2,26 +2,27 @@
 import java.util.*;
 
 class Main{
-
-	public static void fizzBuzz(int n){
-		for(int i=1;i<=n;i++)
-    {
+	    public static void fizzBuzz(int n){
+		    
+		    for(int i=1;i<=n;i++)
+                    {
 			if(i % 3 == 0 && i % 5 == 0){
-        System.out.print("FizzBuzz" + " ");
-      }
+                          System.out.print("FizzBuzz" + " ");
+                         }
 			else if (i % 3 == 0){
-        System.out.print("Fizz" + " ");
-      }
+                          System.out.print("Fizz" + " ");
+                         }
 			else if (i % 5 == 0){
-        System.out.print("Buzz" + " ");
-      }
+                          System.out.print("Buzz" + " ");
+                         }
 			else{
-				System.out.print(i + " ");
+			  System.out.print(i + " ");
 			}
-		}
-	}
-	public static void main(String[] args){
-		int n = 10;
-		fizzBuzz(n);
-	}
-}
+		  }
+	     }
+	
+	     public static void main(String[] args){
+		    int n = 10;
+		    fizzBuzz(n);
+	  }
+  }
