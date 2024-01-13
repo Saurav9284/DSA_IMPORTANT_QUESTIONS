0cc82aa91d67e8f99d42f532f8d9889e1d52194d
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-13 14:28:11 +0530

    Update fizzBuzz.cpp

--- 01_Fizz Buzz/Solution/fizzBuzz.cpp
+++ 01_Fizz Buzz/Solution/fizzBuzz.cpp
@@ -1,18 +1,30 @@
 #include<bits/stdc++.h>
+
 using namespace std;
+
 void fizzBuzz(int n) {
-	// Function to implement Fizz Buzz
-	for (int i = 1; i <= n; i++) {
+	
+	for (int i = 1; i <= n; i++) 
+	{
 		if (i % 3 == 0 && i % 5 != 0)
-			cout << "Fizz" << " ";
+		{
+		   cout << "Fizz" << " ";
+		}	
 		if (i % 5 == 0 && i % 3 != 0)
-			cout << "Buzz" << " ";
+		{
+		   cout << "Buzz" << " ";
+		}	
 		if (i % 5 == 0 && i % 3 == 0)
+		{
 			cout << "FizzBuzz" << " ";
+		}
 		if (i % 5 != 0 && i % 3 != 0)
+		{
 			cout << i << " ";
+		}
 	}
 }
+
 int main() {
 	int n = 10;
 	fizzBuzz(n);
